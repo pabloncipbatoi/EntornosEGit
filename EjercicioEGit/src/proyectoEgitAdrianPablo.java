fdb628b6a6f6d224be7955716ca7c970d6c72e07
Pablo <pabloncipbatoi@gmail.com>
2018-11-13 09:17:42 +0100

    Primer Commit

---
+++ EjercicioEGit/src/proyectoEgitAdrianPablo.java
@@ -0,0 +1,14 @@
+
+public class proyectoEgitAdrianPablo {
+
+	public static void main(String[] args) {
+		
+		String texto = "Hola como estas?";
+		
+		for(int i = 0; i<texto.length();i++) {
+			System.out.println(texto);
+		}
+
+	}
+
+}
